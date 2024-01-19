2a7ca3be91f65ecf11ee2f708bfb16c4d9398be3
Etchmon <etch.rush@gmail.com>
2024-01-19 13:05:35 -0800

    ADD content to header section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,15 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen bg-dark-1">
+    <main className="flex min-h-screen max-w-screen-xl bg-dark-1 mx-auto px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
       <header >
         {/* Header */}
-        <h1 className='text-white-1'>James Etchells</h1>
+        <h1 className='text-4xl sm:text-5xl font-bold tracking-tight text-white-1'>James Etchells</h1>
+        <h2 className='mt-3 text-lg font-medium tracking-tight sm:text-xl text-white-2'>Full-Stack Developer</h2>
+        <p className='mt-4 max-w-xs leading-normal text-frost-3'>I build full-stack applications, design CI/CD pipelines, and love linux</p>
       </header>
-      <section>
+      <article>
+        <section>
         {/* About */}
       </section>
       <section>
@@ -17,6 +20,7 @@ export default function Home() {
       <section>
         {/* Contact */}
       </section>
+      </article>
       <footer>
         {/* Footer */}
       </footer>
